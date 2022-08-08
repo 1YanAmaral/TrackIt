d632bf132abad95842fb847066c59d894a3851c1
Yan Amaral <yan@pop-os.localdomain>
2022-08-08 09:56:40 -0300

    Requisitos OK

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,13 +5,16 @@ import {
   Wrapper,
   Info,
   Bigbutton,
+  Loadbutton,
   SpanLink,
   Logo,
 } from "../styles/sharedStyles";
 import { signup } from "../services/trackItServices";
+import { ThreeDots } from "react-loader-spinner";
 
 export default function SignUpPage() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [form, setForm] = useState({
     email: "",
     name: "",
@@ -29,9 +32,11 @@ export default function SignUpPage() {
 
   function sendSignUp(e) {
     e.preventDefault();
+    setLoading(true);
     const promise = signup(form);
     promise.then((res) => {
       console.log("FOI", res);
+      setLoading(false);
       navigate("/");
     });
     promise.catch(() => alert("Algo está errado, verifique suas informações!"));
@@ -40,42 +45,87 @@ export default function SignUpPage() {
   return (
     <Wrapper>
       <Logo src={logo} alt="TrackIt" />
-      <form onSubmit={sendSignUp}>
-        <Wrapper>
-          <Info
-            type="email"
-            placeholder="email"
-            name="email"
-            onChange={handleForm}
-            value={form.email}
-            required
-          />
-          <Info
-            type="password"
-            placeholder="senha"
-            name="password"
-            onChange={handleForm}
-            value={form.password}
-            required
-          />
-          <Info
-            type="text"
-            placeholder="nome"
-            name="name"
-            onChange={handleForm}
-            value={form.name}
-            required
-          />
-          <Info
-            type="url"
-            placeholder="foto"
-            name="image"
-            onChange={handleForm}
-            value={form.image}
-          />
-          <Bigbutton type="submit">Cadastrar</Bigbutton>
-        </Wrapper>
-      </form>
+      {!loading ? (
+        <form onSubmit={sendSignUp}>
+          <Wrapper>
+            <Info
+              type="email"
+              placeholder="email"
+              name="email"
+              onChange={handleForm}
+              value={form.email}
+              required
+            />
+            <Info
+              type="password"
+              placeholder="senha"
+              name="password"
+              onChange={handleForm}
+              value={form.password}
+              required
+            />
+            <Info
+              type="text"
+              placeholder="nome"
+              name="name"
+              onChange={handleForm}
+              value={form.name}
+              required
+            />
+            <Info
+              type="url"
+              placeholder="foto"
+              name="image"
+              onChange={handleForm}
+              value={form.image}
+            />
+            <Bigbutton type="submit">Cadastrar</Bigbutton>
+          </Wrapper>
+        </form>
+      ) : (
+        <form onSubmit={sendSignUp}>
+          <Wrapper>
+            <Info
+              disabled
+              type="email"
+              placeholder="email"
+              name="email"
+              onChange={handleForm}
+              value={form.email}
+              required
+            />
+            <Info
+              disabled
+              type="password"
+              placeholder="senha"
+              name="password"
+              onChange={handleForm}
+              value={form.password}
+              required
+            />
+            <Info
+              disabled
+              type="text"
+              placeholder="nome"
+              name="name"
+              onChange={handleForm}
+              value={form.name}
+              required
+            />
+            <Info
+              disabled
+              type="url"
+              placeholder="foto"
+              name="image"
+              onChange={handleForm}
+              value={form.image}
+            />
+            <Bigbutton type="submit">
+              <ThreeDots color="white" />
+            </Bigbutton>
+          </Wrapper>
+        </form>
+      )}
       <SpanLink>
         <Link to="/">Já tem uma conta? Faça login!</Link>
       </SpanLink>
